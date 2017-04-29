6d4f6625fb7b9e0e26bef740ee696f2f4570c387
MdAbuNafeeIbnaZahid <1305023.maniz@ugrad.cse.buet.ac.bd>
2017-04-29 20:54:02 +0600

    Created dept model full, teacher model partially

--- mysite/cms/models.py
+++ mysite/cms/models.py
@@ -3,4 +3,18 @@ from django.db import models
 # Create your models here.
 
 
+class Department(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    head = models.ForeignKey('Teacher', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Teacher(models.Model):
+    username = models.CharField(unique=True)
+    password = models.CharField(widget=forms.Pass)
+
+
+
 
