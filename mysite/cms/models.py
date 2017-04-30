3b991e7104aaa4403bee466bec1fa6a5258b2f78
MdAbuNafeeIbnaZahid <1305023.maniz@ugrad.cse.buet.ac.bd>
2017-04-30 21:33:01 +0600

    created model for teacher, student, course

--- mysite/cms/models.py
+++ mysite/cms/models.py
@@ -12,9 +12,35 @@ class Department(models.Model):
 
 
 class Teacher(models.Model):
-    username = models.CharField(unique=True)
-    password = models.CharField(widget=forms.Pass)
+    username = models.CharField(max_length=200, unique=True)
+    password = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200, null=True)
+    last_name = models.CharField(max_length=200, null=True)
+    address = models.CharField(max_length=200, null=True)
+    phone_nom = models.IntegerField(null=True)
+    email_address = models.EmailField(null=True)
+    joinDate = models.DateField(null=True)
+    dept = models.ForeignKey(Department, on_delete=models.CASCADE, null=True)
 
 
+class Student(models.Model):
+    username = models.CharField(max_length=200, unique=True)
+    password = models.CharField(max_length=200)
+    studentId = models.CharField(max_length=200, unique=True)
+    first_name = models.CharField(max_length=200, null=True)
+    last_name = models.CharField(max_length=200, null=True)
+    address = models.CharField(max_length=200, null=True)
+    phone_nom = models.IntegerField(null=True)
+    email_address = models.EmailField(null=True)
+    dept = models.ForeignKey(Department, on_delete=models.CASCADE, null=True)
+    level = models.IntegerField(null=True)
+    term = models.IntegerField(null=True)
+
+
+class Course(models.Model):
+    course_num = models.CharField(max_length=200, null=True)
+    course_name = models.CharField(max_length=200, null=True)
+    credi_hour = models.IntegerField(null=True)
+    dept = models.ForeignKey(Department, on_delete=models.CASCADE, null=True)
 
 
