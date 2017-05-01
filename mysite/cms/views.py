5beda02e8f7f32c5bfaa244674b57a51b42e67dd
MdAbuNafeeIbnaZahid <1305023.maniz@ugrad.cse.buet.ac.bd>
2017-05-01 17:21:33 +0600

    Created a navigation template for student, general login page for students and teacher. Added a login url

--- mysite/cms/views.py
+++ mysite/cms/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse, Http404
+import datetime
+from random import randint
+from django.template.loader import get_template
+from django.template import Context, Template
+from django.shortcuts import render
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from books.models import Book
+from mysite.forms import ContactForm
+from django.http import HttpResponseRedirect
+from django.core.mail import send_mail
+
+from cms.models import Teacher, Student
+
+
 # Create your views here.
+
+def handle_log_in(request):
+    username = request.GET.get('username', '')
+    password = request.GET.get('password', '')
+    usertype = request.GET.get('usertype', '')
+
+    print(username)
+    print(password)
+    print(usertype)
+
+    if ( usertype == "teacher" ):
+        #Teacher =
+        pass
+
+    elif ( usertype == 'student' ):
+        pass
+
+    return render(request, 'login_page.html')
+
+
