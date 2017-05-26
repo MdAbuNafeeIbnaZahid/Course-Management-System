1c5952f1bdbfac6d41b5def55dd4b4b8a0a8965d
MdAbuNafeeIbnaZahid <1305023.maniz@ugrad.cse.buet.ac.bd>
2017-05-27 00:59:07 +0600

    Now the students can download the files uploaded by teachers in forum post. But downloaded file becoming distorted

--- mysite/cms/admin.py
+++ mysite/cms/admin.py
@@ -4,37 +4,37 @@ from .models import Department, Teacher, Student, Course, Class_of_course, \
     Enrolment, Question_of_vote, Option_of_vote, Response_of_vote, User, Forum_post
 
 
-class StudentAdminForm(ModelForm):
-    class Meta:
-        model = Student
-        widgets = {
-            'password' : PasswordInput(),
-        }
-        fields = "__all__"
-
-
-class TeacherAdminForm(ModelForm):
-    class Meta:
-        model = Teacher
-        # widgets = {
-        #     'password' : PasswordInput(),
-        # }
-        fields = "__all__"
-
-
-class StudentAdmin(admin.ModelAdmin):
-    form = StudentAdminForm
-
-
-class TeacherAdmin(admin.ModelAdmin):
-    form = TeacherAdminForm
+# class StudentAdminForm(ModelForm):
+#     class Meta:
+#         model = Student
+#         widgets = {
+#             'password' : PasswordInput(),
+#         }
+#         fields = "__all__"
+#
+#
+# class TeacherAdminForm(ModelForm):
+#     class Meta:
+#         model = Teacher
+#         # widgets = {
+#         #     'password' : PasswordInput(),
+#         # }
+#         fields = "__all__"
+#
+#
+# class StudentAdmin(admin.ModelAdmin):
+#     form = StudentAdminForm
+#
+#
+# class TeacherAdmin(admin.ModelAdmin):
+#     form = TeacherAdminForm
 
 # Register your models here.
 
 admin.site.register( User )
 admin.site.register( Department )
-admin.site.register( Teacher, TeacherAdmin )
-admin.site.register( Student, StudentAdmin )
+admin.site.register( Teacher )
+admin.site.register( Student )
 admin.site.register( Course )
 admin.site.register( Class_of_course )
 admin.site.register( Enrolment )
